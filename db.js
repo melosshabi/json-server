82b0c6d8707584cf6449e6f5ca900b2266dc39b0
melosshabi <melosshabi05@gmail.com>
2022-11-05 22:32:58 +0100

    update

--- db.js
+++ db.js
@@ -1,13 +1,7 @@
-const items = require("./data/items");
-const tags = require("./data/tags");
-const brands = require("./data/brands");
-const itemTypes = require("./data/itemTypes");
+const items = require("./data/users");
 
 module.exports = {
-  items,
-  tags,
-  brands,
-  itemTypes
+users
 }
 
 
